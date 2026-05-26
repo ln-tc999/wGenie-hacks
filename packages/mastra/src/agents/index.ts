d272473e153a8271d3e50c07b45daf51180c287c
maulana-tech <firdaussyah03@gmail.com>
2026-05-26 09:39:12 +0700

    refactor(packages/mastra/src/agents/index.ts): auto update

--- packages/mastra/src/agents/index.ts
+++ packages/mastra/src/agents/index.ts
@@ -1,4 +1,4 @@
 export { plasmaVaultAgent } from './plasma-vault-agent';
 export { sqlAgent } from './sql-agent';
 export { alphaAgent } from './alpha-agent';
-export { yoTreasuryAgent } from './yo-treasury-agent';
+export { wgenieCfoAgent } from './wgenie-cfo-agent';
