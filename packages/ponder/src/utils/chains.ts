ccd87c073e3e631865717def2ee2e2fb1eb9569e
maulana-tech <firdaussyah03@gmail.com>
2026-05-27 20:59:10 +0700

    refactor(packages/ponder/src/utils/chains.ts): auto update

--- packages/ponder/src/utils/chains.ts
+++ packages/ponder/src/utils/chains.ts
@@ -1,7 +1,7 @@
-import { arbitrum, base, mainnet, plasma, unichain } from 'viem/chains';
+import { arbitrum, base, mainnet, mantle, plasma, unichain } from 'viem/chains';
 import { z } from 'zod';
 
-export const chains = [mainnet, arbitrum, base, unichain, plasma] as const;
+export const chains = [mainnet, arbitrum, base, unichain, plasma, mantle] as const;
 
 export const chainIdSchema = z.union([
   z.literal(mainnet.id),
@@ -9,6 +9,7 @@ export const chainIdSchema = z.union([
   z.literal(base.id),
   z.literal(unichain.id),
   z.literal(plasma.id),
+  z.literal(mantle.id),
 ]);
 
 export type ChainId = z.infer<typeof chainIdSchema>;
