5c3b4a4be9672f44062f58cf3fceb6bfb799649c
maulana-tech <firdaussyah03@gmail.com>
2026-06-03 19:58:16 +0700

    refactor(packages/mastra/src/tools/wgenie-cfo/index.ts): auto update

--- packages/mastra/src/tools/wgenie-cfo/index.ts
+++ packages/mastra/src/tools/wgenie-cfo/index.ts
@@ -1,6 +1,7 @@
 export { createMerchantMoeSwapActionTool } from './create-swap-action';
 export { readTreasuryBalancesTool } from './read-treasury-balances-tool';
 export { readTreasuryBalances } from './read-treasury-balances';
+export { readWalletGenieTreasuryTool } from './read-walletgenie-balance-tool';
 export type {
   TreasuryAsset,
   MantlePosition,
